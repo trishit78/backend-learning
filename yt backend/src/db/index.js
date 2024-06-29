4b22dbcc9235fbf3351bac477b7dbb0abc377e9e
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2024-06-29 08:28:05 +0530

    mongodb connected

---
+++ yt backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+
+import { DB_NAME } from "../constants.js"
+
+//console.log(process.env.MONGODB_URI)
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection error",error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
