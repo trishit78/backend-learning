5aa1786206fa68388b7781934f501819e838be85
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2024-06-30 08:07:57 +0530

    Day3

--- yt backend/src/index.js
+++ yt backend/src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 // import mongoose from "mongoose"
 // import { DB_NAME } from "./constants"
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js"
 
 dotenv.config({
@@ -12,7 +12,15 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed !!",err);
+})
 
 
 
